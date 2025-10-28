0f038ae5ecea0b93f08e203b5f082c76901d5e4a
Joel Ross <joelross@uw.edu>
2025-10-28 12:20:30 -0700

    End of es6 demo

--- es6/js/index.js
+++ es6/js/index.js
@@ -1,6 +1,14 @@
 'use strict';
 
-//console.log("executing main.js")
+import { otherMsg } from './other.js';
+
+import {otherFunc} from './other.js';
+
+//import default
+import otherFunc from './other.js';
+
+
+console.log("executing main.js")
 
 //an array of objects to work with
 const peopleArray = [
@@ -10,4 +18,49 @@ const peopleArray = [
   {name: 'Diya', height: 69, weight: 144},
   {name: 'Emma', height: 71, weight: 152}
 ]
-//console.log(peopleArray);
+console.log(peopleArray);
+
+console.log(otherMsg);
+
+otherFunc();
+
+// const foo = (param) => {
+//   return 'hello world';
+// }
+
+// console.log(foo("hello"));
+
+
+// const myArray = [1, 2, 3];
+// const [x, y, z, a] = myArray;
+
+// const myObject = {a: 1, b: 2, c: 3, d: 4};
+// const {b, a, c, e} = myObject;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(e);
+
+// const personObj = {name: 'Joel', job: "Prof"}
+// const {name, prof} = personObj;
+
+
+// function bar({a, b}, {c, d}) {
+//   console.log(a, b, c, d);
+// }
+
+// bar({a:1, b:2,}, {c: 3, d:4});
+
+
+
+
+
+
+
+// function getFullName({first, last}) {
+//   return first + " " + last;
+// }
+
+// getFullName("joel", "ross");
+
--- es6/js/other.js
+++ es6/js/other.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log("executing other.js");
+
+const peopleArray = ["John", "Paul", "George", "Ringo"];
+
+console.log(peopleArray[0]);
+
+export const otherMsg = "Hello from other";
+
+export default function otherFunc() {
+  console.log("Other is best module");
+}
+
